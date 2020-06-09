0e967e43e1b65d48c8152be64e7ff994767993c7
meltingshine <meltingshine@gmail.com>
2020-06-09 21:10:50 +0900

    Glide 웹상 이미지 불러오기 -> Manifest에서 인터넷 권한주기 -> http는 안됨 https 는 됨

--- app/src/main/java/com/example/library2nd_20200609/MainActivity.kt
+++ app/src/main/java/com/example/library2nd_20200609/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.library2nd_20200609
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -16,6 +18,7 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+    Glide.with(mContext).load("https://image.ytn.co.kr/general/jpg/2020/0211/202002111418090858_d.jpg").into(profileImg)
     }
 
     override fun setupEvents() {
