0cb9997cae1d4dcac9e92e929d99b4e37e872119
meltingshine <meltingshine@gmail.com>
2020-06-09 20:21:19 +0900

    PhotoView 활용 예시

---
+++ app/src/main/java/com/example/library2nd_20200609/LargePhotoActivity.kt
@@ -0,0 +1,22 @@
+package com.example.library2nd_20200609
+
+import android.os.Bundle
+
+class LargePhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_large_photo)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
--- app/src/main/java/com/example/library2nd_20200609/MainActivity.kt
+++ app/src/main/java/com/example/library2nd_20200609/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.library2nd_20200609
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -17,8 +19,15 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        profileImg.setOnClickListener {
 
+            val myIntent = Intent(mContext, LargePhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+    }
     }
 
 
-}
+
